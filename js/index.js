bc7b782bfb567da806c1234f8a6ed8a71c916cfa
Abdukarim01 <84226194+Abdukarim01@users.noreply.github.com>
2021-10-10 01:39:26 -0700

    new commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const myApp = {
+const myApp1 = {
   data(){
     return{
       nav:false
@@ -13,5 +13,38 @@ const myApp = {
       }
   }
 };
-const app = Vue.createApp(myApp);
+const app = Vue.createApp(myApp1);
 app.mount('#section')
+
+let logs = document.querySelector('.right-menu-in')
+let num = 0
+let num2 = 1
+logs.addEventListener('click',()=>{
+  let arr = ['img/logo1.svg','img/logo2.svg']
+  let arrcolor = ['green','white']
+  num++
+  logs.style = `background-image:url(${arr[num]});
+                background-color:${arrcolor[num]};`
+  if(num >= 1){
+    num = -1}
+
+num2++
+
+let logo = document.querySelectorAll('.bg')
+
+if(num2 === 2){
+  logo.forEach((l)=>{
+    l.classList = 'bg bg-logos'
+  })
+  num2 = 0
+}
+
+
+if(num2 === 1){
+  logo.forEach((l)=>{
+    l.classList = 'bg linear'
+  })
+}
+
+
+})
